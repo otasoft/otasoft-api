e9c4bf6b8961fbee92c7e48f2595bf7da96e2992
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-10-13 21:50:40 +0200

    feat-#49: configure Otasoft API for https
    - add rule to env.example to serve locally with SSL
    
    - add rule to gitignore to ignore local public cert and private key stored inside secrets directory
    
    - add conditional http/https Nest application creation

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 import { swaggerOptions } from './doc/swagger-options';
-import { rateLimitConfigObject } from './security/rateLimitConfig';
+import { rateLimitConfigObject } from './security/configs/rateLimitConfig';
+import { httpsOptions } from './security/configs/httpsOptions';
 
 (async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = process.env.SERVE_LOCAL_SSL
+    ? await NestFactory.create(AppModule, { httpsOptions })
+    : await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
   if (process.env.ENVIRONMENT === 'development') {
@@ -15,8 +18,7 @@ import { rateLimitConfigObject } from './security/rateLimitConfig';
     SwaggerModule.setup('doc', app, document);
   } else {
     app.use(helmet());
-    const rateLimiter = rateLimit(rateLimitConfigObject);
-    app.use(rateLimiter);
+    app.use(rateLimit(rateLimitConfigObject));
   }
 
   await app.listen(3000);
