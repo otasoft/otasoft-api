42ae23b87413f3593df40bef99db908def2f766a
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-10-20 20:28:43 +0200

    feat-#61: create frontend cookie guard
    - install cookie-parser and configure it in main.ts
    
    - add FRONTEND_COOKIE to .env.example
    
    - create FrontendCookieGuard to make sure that the requests are sent from valid location

---
+++ src/security/guards/frontend-cookie.guard.spec.ts
@@ -0,0 +1,7 @@
+import { FrontendCookieGuard } from './frontend-cookie.guard';
+
+describe('FrontendCookieGuard', () => {
+  it('should be defined', () => {
+    expect(new FrontendCookieGuard()).toBeDefined();
+  });
+});
