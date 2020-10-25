cd33b6b3c3524d2ed71ea1d71c1bdf3b485fe00d
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-10-25 14:09:46 +0100

    mile-#43: configure main.ts to use redis session

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,26 @@ import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
 import * as compression from 'compression';
 import * as cookieParser from 'cookie-parser';
+import * as passport from 'passport';
+
 import { AppModule } from './app.module';
 import { swaggerOptions } from './doc/swagger-options';
 import { rateLimitConfigObject } from './security/configs/rateLimitConfig';
 import { httpsOptions } from './security/configs/httpsOptions';
 import { FrontendCookieGuard } from './security/guards/frontend-cookie.guard';
+import { createRedisSession } from './security/configs/redisSessionConfig';
 
 (async function bootstrap() {
   const app = process.env.SERVE_LOCAL_SSL
     ? await NestFactory.create(AppModule, { httpsOptions })
     : await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.use(cookieParser());
   app.useGlobalGuards(new FrontendCookieGuard());
+
+  app.use(cookieParser());
+  app.use(createRedisSession());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   if (process.env.ENVIRONMENT === 'development') {
     const document = SwaggerModule.createDocument(app, swaggerOptions);
