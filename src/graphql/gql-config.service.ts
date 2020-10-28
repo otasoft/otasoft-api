939c7dccc478fc03c27f4b2d161b9e9ff52c3565
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-10-28 18:46:14 +0100

    refactor-#67: configure GraphQL to use catalog microservice

--- src/graphql/gql-config.service.ts
+++ src/graphql/gql-config.service.ts
@@ -2,13 +2,14 @@ import { GqlModuleOptions, GqlOptionsFactory } from '@nestjs/graphql';
 import { join } from 'path';
 import { AuthModule } from 'src/microservices/auth/auth.module';
 import { BookingModule } from 'src/microservices/booking/booking.module';
+import { CatalogModule } from 'src/microservices/catalog/catalog.module';
 import { CustomerModule } from 'src/microservices/customer/customer.module';
 import { MailModule } from 'src/microservices/mail/mail.module';
 
 export class GqlConfigService implements GqlOptionsFactory {
   createGqlOptions(): GqlModuleOptions {
     return {
-      include: [AuthModule, CustomerModule, MailModule, BookingModule],
+      include: [AuthModule, CustomerModule, MailModule, BookingModule, CatalogModule],
       autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
     };
   }
