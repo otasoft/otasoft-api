c0c0743a4e6a56f7d80ad6ff851d2c5aad44a488
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-11-21 21:51:38 +0100

    feat-#64: add Cookie based sign in for REST architecture

--- src/microservices/auth/rest/controllers/auth/auth.controller.ts
+++ src/microservices/auth/rest/controllers/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { AuthService } from '../../../services/auth/auth.service';
 import { AuthCredentialsDto } from '../../dto/auth-credentials.dto';
 import { RestAuthUserToken } from '../../models/auth-user-token-rest.model';
 import { RestAuthUser } from '../../models/auth-user-rest.model';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -18,7 +19,12 @@ export class AuthController {
   @Post('/signin')
   async signIn(
     @Body() authCredentialsDto: AuthCredentialsDto,
-  ): Promise<RestAuthUserToken> {
-    return this.authService.signIn(authCredentialsDto);
+    @Res() response: Response
+  ): Promise<Response<RestAuthUserToken>> {
+    const cookieObject = await this.authService.signIn(authCredentialsDto);
+
+    response.setHeader('Set-Cookie', cookieObject.cookie);
+
+    return response.send(cookieObject);
   }
 }
--- src/microservices/auth/rest/models/auth-user-token-rest.model.ts
+++ src/microservices/auth/rest/models/auth-user-token-rest.model.ts
@@ -2,5 +2,5 @@ import { IsString } from 'class-validator';
 
 export class RestAuthUserToken {
   @IsString()
-  accessToken: string;
+  cookie: string;
 }
