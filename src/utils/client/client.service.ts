333265d6431f3b7a22b9e51bbed4b9689418ee6a
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-09 12:38:09 +0100

    feat-#101: create client service and client options config

---
+++ src/utils/client/client.service.ts
@@ -0,0 +1,27 @@
+import { HttpException, Injectable } from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
+import { IMessagePattern } from "./interfaces";
+
+@Injectable()
+export class ClientService {
+  /**
+   * Method used to send the request to the corresponding microservice. It accepts following parameters:
+   *
+   * - @param {ClientProxy} client - microservice client to send the message to
+   * - @param {IMessagePattern} messagePattern - object containing the pattern for a message (i.e. `{ role: 'user', cmd: 'create' }`)
+   * - @param {*} payload - data to send to the microservice client
+   *
+   * @return {*}  {Promise<any>} - returned response from a microservice or an adequate HTTP exception
+  */
+  async sendRequest(
+    client: ClientProxy,
+    messagePattern: IMessagePattern,
+    payload: any,
+  ): Promise<any> {
+    try {
+      return await client.send(messagePattern, payload).toPromise();
+    } catch (error) {
+      throw new HttpException(error.errorStatus, error.statusCode);
+    }
+  }
+}
---
+++ src/utils/client/interfaces/message-pattern.interface.ts
@@ -0,0 +1,14 @@
+/**
+ * Interface for message pattern used to send request to microservice
+ *
+ * - @property {string} role
+ * - @property {string} cmd
+ * - @property {*} metadata?
+ *
+ */
+export interface IMessagePattern {
+    role: string;
+    cmd: string;
+    metadata?: any;
+  }
+  
