b22dee864e30ad879998262949b3f7aadb6aa352
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-11 11:43:40 +0100

    feat-#44: create dto and input for setNewPassword

--- src/microservices/auth/graphql/input/index.ts
+++ src/microservices/auth/graphql/input/index.ts
@@ -1,3 +1,4 @@
 export * from './auth-credentials.input';
 export * from './auth-email.input';
 export * from './change-password.input';
+export * from './set-new-password.input';
--- src/microservices/auth/rest/dto/index.ts
+++ src/microservices/auth/rest/dto/index.ts
@@ -2,3 +2,4 @@ export * from './auth-credentials.dto';
 export * from './auth-email.dto';
 export * from './change-password.dto';
 export * from './get-refresh-user.dto';
+export * from './set-new-password.dto';
