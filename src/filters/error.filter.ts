fab225c73c0c389bf74b8643cfe8055dbaed3a2f
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-26 13:48:47 +0100

    feat-#118: fix problem with other exception throws

--- src/filters/error.filter.ts
+++ src/filters/error.filter.ts
@@ -14,16 +14,31 @@ import { validateServerError } from './helpers';
 @Catch()
 export class ErrorFilter implements ExceptionFilter {
   catch(error, host: ArgumentsHost) {
+    let request = host.switchToHttp().getRequest();
     let response = host.switchToHttp().getResponse();
     let status =
       error instanceof HttpException
         ? error.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
+    let statusCode;
+    let errorResponse;
+
     if (status === HttpStatus.INTERNAL_SERVER_ERROR) {
       const { code, message } = validateServerError(error.code);
 
-      return response.status(code).send(message);
+      statusCode = code;
+      errorResponse = message;
+    } else {
+      statusCode = status;
+      errorResponse = error.getResponse();
     }
+
+    return response.status(status).json({
+      statusCode,
+      errorResponse,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    })
   }
 }
