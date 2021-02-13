92f5330afc937bc3511f007d3bb2ea59f666620f
Adam Oleszko <adamoleszko44@gmail.com>
2021-02-13 19:13:27 +0100

    feat-#4: Add amount and card_token

--- src/microservices/payment/graphql/input/create-payment.input.ts
+++ src/microservices/payment/graphql/input/create-payment.input.ts
@@ -1,9 +1,17 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 @InputType()
 export class CreatePaymentInput {
   @Field()
   @IsNumber()
   booking_id: number;
+
+  @Field()
+  @IsNumber()
+  amount: number;
+
+  @Field()
+  @IsString()
+  card_token: number;
 }
--- src/microservices/payment/rest/dto/create-payment.dto.ts
+++ src/microservices/payment/rest/dto/create-payment.dto.ts
@@ -1,6 +1,12 @@
-import { IsPositive } from 'class-validator';
+import { IsPositive, IsString, Min } from 'class-validator';
 
 export class CreatePaymentDto {
   @IsPositive()
   booking_id: number;
+
+  @IsString()
+  card_token: string;
+
+  @Min(200)
+  amount: number;
 }
