4f24eaa30b08b2b5cf57afffd759b962581ded5b
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-02-15 09:16:13 +0100

    refactor-#155: copy logic of open-id controllers and services

--- src/open-id/controllers/open-id.controller.ts
+++ src/open-id/controllers/open-id.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+
+import { OpenIdService } from '../services';
 
 @Controller('open-id')
-export class OpenIdController {}
+export class OpenIdController {
+  constructor(private readonly openIdService: OpenIdService) {}
+
+//   @UseGuards(OpenIDGuard)
+  @Get('/login')
+  login() {
+    return this.openIdService.login();
+  }
+
+  @Get('/user')
+  user(@Req() req) {
+    return this.openIdService.user(req);
+  }
+
+//   @UseGuards(OpenIDGuard)
+  @Get('/callback')
+  loginCallback(@Res() res: Response) {
+    return this.openIdService.loginCallback(res);
+  }
+
+  @Get('/logout')
+  async logout(@Req() req, @Res() res: Response) {
+    return this.openIdService.logout(req, res);
+  }
+}
