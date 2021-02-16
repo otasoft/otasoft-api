26ad851468f43ade17ab91721ab79355c8f20ccc
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-02-16 09:08:26 +0100

    test-#145: use clientService in booking service

--- src/microservices/booking/services/booking.service.ts
+++ src/microservices/booking/services/booking.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable, HttpException } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
+import { ClientService } from '@utils/client';
 import { CreateBookingDto } from '../rest/dto';
 import { RestBooking } from '../rest/models';
 
@@ -9,56 +10,41 @@ export class BookingService {
   constructor(
     @Inject('BOOKING_MICROSERVICE')
     public readonly bookingClient: ClientProxy,
+    private readonly clientService: ClientService,
   ) {}
 
   async getBookingById(id: number): Promise<RestBooking> {
-    try {
-      const booking = await this.bookingClient
-        .send({ role: 'booking', cmd: 'get' }, id)
-        .toPromise();
-
-      return booking;
-    } catch (error) {
-      throw new HttpException(error.errorStatus, error.statusCode);
-    }
+    return this.clientService.sendMessageWithPayload(
+      this.bookingClient,
+      { role: 'booking', cmd: 'get' },
+      id,
+    );
   }
 
   async createBooking(newBooking: CreateBookingDto): Promise<RestBooking> {
-    try {
-      const booking = await this.bookingClient
-        .send({ role: 'booking', cmd: 'create' }, newBooking)
-        .toPromise();
-
-      return booking;
-    } catch (error) {
-      throw new HttpException(error.errorStatus, error.statusCode);
-    }
+    return this.clientService.sendMessageWithPayload(
+      this.bookingClient,
+      { role: 'booking', cmd: 'create' },
+      newBooking,
+    );
   }
 
   async updateBooking(
     id: number,
     updatedBooking: CreateBookingDto,
   ): Promise<RestBooking> {
-    try {
-      const booking = await this.bookingClient
-        .send({ role: 'booking', cmd: 'update' }, { id, updatedBooking })
-        .toPromise();
-
-      return booking;
-    } catch (error) {
-      throw new HttpException(error.errorStatus, error.statusCode);
-    }
+    return this.clientService.sendMessageWithPayload(
+      this.bookingClient,
+      { role: 'booking', cmd: 'update' },
+      { id, updatedBooking },
+    );
   }
 
   async deleteBookingById(id: number): Promise<RestBooking> {
-    try {
-      const booking = await this.bookingClient
-        .send({ role: 'booking', cmd: 'remove' }, id)
-        .toPromise();
-
-      return booking;
-    } catch (error) {
-      throw new HttpException(error.errorStatus, error.statusCode);
-    }
+    return this.clientService.sendMessageWithPayload(
+      this.bookingClient,
+      { role: 'booking', cmd: 'remove' },
+      id,
+    );
   }
 }
